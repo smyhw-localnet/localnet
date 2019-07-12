f7a93165a205f7b3034dcbe0360cab9afe0066f4
smyhw <i@smyhw.online>
2019-07-12 09:53:52 +0800

    修复了to命令的分解bug

---
+++ command.java
@@ -0,0 +1,109 @@
+package online.smyhw.localnet;
+
+import java.net.*;
+import java.io.*;
+
+public class command
+{
+	public static void local(String command) throws Exception
+	{
+		message.info("[命令解释器]:开始解析:"+command);
+		switch(fj(command,0))
+		{
+		case "test":
+			command_test.main(command);
+			break;
+		case "to":
+			command_to.main(command);
+			break;
+		default:
+			message.show("未知指令!");
+		}
+	}
+	public static void net(String ID,String command)
+	{
+		switch(fj(command,0))
+		{
+		case "test":
+			command_test.main(command);
+			break;
+		case "show":
+			message.show("来自 "+ID+"的文本信息："+fj(command,1));
+			break;
+		default:
+			message.show(ID+"传输了一个未知指令："+command);
+		}
+	}
+	static String fj(String command,int num)
+	{
+		String re=" ";
+		int sy=0;//索引，即现在检查到了哪个字符，以0开头
+		int dl=-1;//记录现在检查到了第几个参数，以1开头
+		int s=0,e=-1;//当前参数开始处索引以及结束处索引(e=结束处索引+1)
+		command=command.trim();//删除前后的空白
+		if(command.isEmpty())
+		{
+			message.info("[命令解释器]:空指令！");
+			return " ";
+		}
+		command=command+" ";//尾部加空格，防止单节指令无法读到空格而出错
+		//添加特殊功能
+		if(num==-1)//砍掉指令第一段
+		{
+			return command.substring(command.indexOf(' ')+1);
+		}
+		//一般分解过程
+		while(true)
+		{
+			if(command.length()==sy){break;}
+			if(command.charAt(sy)==' ')
+			{
+				s=e+1;
+				e=sy;
+				dl++;
+				if(dl==num)//返回字符串 
+				{
+					re=command.substring(s,e);
+					return re;
+				}
+			}
+			sy++;
+		}
+		return " ";
+	}
+}
+
+
+
+class command_test
+{
+	public static void main(String input)
+	{
+		message.show("test指令执行成功");
+	}
+}
+
+class command_to
+{
+	static String ID,temp;
+	public static void main(String input) throws Exception
+	{
+		message.info("尝试连接至"+command.fj(input,1)+":4201");
+		Socket client = new Socket(command.fj(input,1),4201);
+		message.info("连接成功，尝试建立数据流");
+		DataInputStream in = new DataInputStream(client.getInputStream());
+		DataOutputStream out = new DataOutputStream(client.getOutputStream());
+		message.info("数据流建立成功，读取对方ID");
+		ID=in.readUTF();
+		message.show(command.fj(input,1)+"报告他的ID为："+ID);
+		message.show("向"+ID+"发送本终端ID"+localnet.ID);
+		out.writeUTF(localnet.ID);
+		temp=in.readUTF();
+		message.info(command.fj(input,1)+"(报告ID为"+ID+")发来报文"+temp);
+		if(temp.equals("OK"))
+		{
+			out.writeUTF(command.fj((command.fj(input,-1)),-1));
+		}
+		client.close();
+	}
+}
