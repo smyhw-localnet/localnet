019edbe409f63e4bc4e3eac4da3c1e391c2332d4
smyhw <i@smyhw.online>
2020-07-03 18:36:01 +0800

    “DocManager”

--- src/online/smyhw/localnet/data/DocManager.java
+++ src/online/smyhw/localnet/data/DocManager.java
@@ -1,5 +1,13 @@
 package online.smyhw.localnet.data;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+import online.smyhw.localnet.LN;
+import online.smyhw.localnet.message;
+
 /**
  * 该类用于读取文档内容
  * @author smyhw
@@ -7,20 +15,36 @@ package online.smyhw.localnet.data;
  */
 public class DocManager 
 {
+	static HashMap<String,String> RunTimeDoc = new HashMap<String,String>();
 	/**
 	 * 从指定JAR路径获取文档文本
 	 * @param path文档路径
-	 * @return 文档文本
+	 * @return 文档文本,若文档不存在或读取出错，返回null
 	 */
 	public String readFromJar(String path)
 	{
-		return new String();
+		String re = "";
+		try 
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(LN.class.getResourceAsStream(path),"utf-8"));
+			while(true)
+			{
+				String temp1 = br.readLine();
+				if(temp1==null) {break;}
+				re = re +"\n"+temp1;
+			}
+		}catch (Exception e) 
+		{
+			message.warning("DocManager读取JAR中文档错误", e);
+			return null;
+		}
+		return re;
 	}
 	
 	/**
 	 * 从指定文件路径获得文档文本
 	 * @param path 文件路径
-	 * @return 文档文本
+	 * @return 文档文本,若文档不存在，返回null
 	 */
 	public String readFromFile(String path)
 	{
@@ -30,22 +54,24 @@ public class DocManager
 	/**
 	 * 根据给定的名称获得文档
 	 * @param path 文档名称
-	 * @return 文档文本
+	 * @return 文档文本,若文档不存在，返回null
 	 */
 	public String readFromRunTime(String path)
 	{
-		return new String();
+		return RunTimeDoc.get(path);
 	}
 	
 	/**
 	 * 根据给定名称设定RunTime文档</br>
 	 * RunTime文档会覆盖其他相同路径的文档</br>
+	 * 
 	 * @param doc 文档文本
 	 * @param path 文档名称
 	 * @return 如果设置成功即返回true，否则返回false
 	 */
 	public boolean setRunTimeDoc(String doc,String path)
 	{
+		RunTimeDoc.put(path, doc);
 		return false;
 	}
 	/**
@@ -54,7 +80,7 @@ public class DocManager
 	 * JAR中的文档优先级高于文件</br>
 	 * RunTime中的文档优先级高于JAR
 	 * @param path 文档路径
-	 * @return 文档文本
+	 * @return 文档文本,若文档不存在，返回null
 	 */
 	public String getDoc(String path)
 	{
